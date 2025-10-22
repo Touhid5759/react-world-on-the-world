0622ff68ca778571894e03afdbc81eee64e34dd6
Touhid5759 <touhid15-5759@diu.edu.bd>
2025-10-22 17:32:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Countries from './component/countries/countries'
+function App() {
+
+  return (
+    <>
+        <p>
+          <Countries></Countries>
+        </p>
+    </>
+  )
+}
+
+export default App
